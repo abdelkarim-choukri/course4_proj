25be6f08e5dc97e2d325b8ac754758d12ff34965
abdelkarim-choukri <choukriabde98@gmail.com>
2023-03-21 11:44:17 +0000

    Finish django signals

--- course4_proj/settings.py
+++ course4_proj/settings.py
@@ -157,3 +157,5 @@ class Dev(Configuration):
     CELERY_RESULT_BACKEND = "django-db"
     CELERY_BROKER_URL = "redis://localhost:6379/0"
     # "53514c97" "5cbfa15c"    
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    ADMINS = [("Ben", "ben@example.com")]
